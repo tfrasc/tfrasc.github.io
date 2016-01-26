55e25844f7b90930a0de1fd6de99b88a45dede7e
tfrasc <tfrasc@gmail.com>
2016-01-25 20:18:41 -0800

    Add IG button

---
+++ instafeed.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+  var feed = new Instafeed({
+  get: 'user',
+  limit: 'count',
+  resolution: 'standard_resolution',
+  userId: '2174415839',
+  clientId: '5ecff2e1873b4d519763b8f3adf4bc70',
+  template: '<a href="{{link}}"><img src="{{image}}"style="width: 280px; height: 280px; border-radius: 50px; padding-top: 10px; padding-left: 10px; display: inline-block;"/></a>'
+  });
+  feed.run();
+</script>
